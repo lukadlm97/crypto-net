e711fd5b75a970f39384c8318654de36a6b68bb6
Luka Radovanovic <lukadlm97@gmail.com>
2020-06-03 00:17:02 +0200

    created simple randomNumber generator

---
+++ crypto-dotnet.RecommendedRandomGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace crypto_dotnet.RecommendedRandomGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 20; i++)
+            {
+                string randomNumber =
+                    Convert.ToBase64String(Random.GenerateRandomNumber(20));
+                Console.WriteLine(randomNumber);
+            }
+        }
+    }
+}
---
+++ crypto-dotnet.RecommendedRandomGenerator/Random.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace crypto_dotnet.RecommendedRandomGenerator
+{
+    public class Random
+    {
+        public static byte[] GenerateRandomNumber(int lenght)
+        {
+            using(var randomGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[lenght];
+                randomGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+    }
+}
