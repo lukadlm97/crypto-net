42a7fc975d6ebf50f68f5fc67261b24ed8963e59
Luka Radovanovic <lukadlm97@gmail.com>
2020-06-03 00:47:52 +0200

    md4 algorithm discovered

---
+++ crypto-dotnet.RecommendedRandomGenerator/HashData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace crypto_dotnet.RecommendedRandomGenerator
+{
+    class HashData
+    {
+        public static byte[] ComputeHashMd5(byte[] toBeHashed)
+        {
+            using (var md5 = MD5.Create())
+            {
+                return md5.ComputeHash(toBeHashed);
+            }
+        }
+    }
+}
--- crypto-dotnet.RecommendedRandomGenerator/Program.cs
+++ crypto-dotnet.RecommendedRandomGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace crypto_dotnet.RecommendedRandomGenerator
 {
@@ -6,12 +7,22 @@ namespace crypto_dotnet.RecommendedRandomGenerator
     {
         static void Main(string[] args)
         {
-            for(int i = 0; i < 20; i++)
-            {
-                string randomNumber =
-                    Convert.ToBase64String(Random.GenerateRandomNumber(20));
-                Console.WriteLine(randomNumber);
-            }
+            //for(int i = 0; i < 20; i++)
+            //{
+            //    string randomNumber =
+            //        Convert.ToBase64String(Random.GenerateRandomNumber(20));
+            //    Console.WriteLine(randomNumber);
+            //}
+
+            string originalMessage1 = " Message to hash";
+            string originalMessage2 = " Message to hash";
+
+            var md5HashedMessage1 = HashData.ComputeHashMd5(Encoding.UTF8.GetBytes(originalMessage1));
+            var md5HashedMessage2 = HashData.ComputeHashMd5(Encoding.UTF8.GetBytes(originalMessage2));
+
+            Console.WriteLine("Message 1 hash "+Convert.ToBase64String(md5HashedMessage1));
+            Console.WriteLine("Message 2 hash "+Convert.ToBase64String(md5HashedMessage2));
+
         }
     }
 }
